3eee0d605f59f93a195524d356fd3743b14b81d1
sakshi26-97 <sakshisubedi@gmail.com>
2022-03-23 14:17:20 +0530

    feat: chess validator

---
+++ chess-validator/README.md
@@ -0,0 +1,31 @@
+## Chess Validator
+
+### Problem Link
+https://workat.tech/machine-coding/practice/design-chess-validator-to77d8oqpx2h
+
+### Requirements
+- 2 players play the game.
+- Player with white pieces play first.
+- A player may move only to an empty cell unless it is trying to capture a piece of the other player.
+- A piece is captured when a player moves to a position currently occupied by a piece of the opponent.
+- For invalid move, same player plays again.
+- The user will make a move by entering the start position and the end position.
+
+### Optional Requirements
+- [ ] Grid size customisable.
+- [ ] Start position customisable.
+- [ ] Allow new types of pieces with any type of move.
+- [ ] Change the move style of any existing pieces.
+
+
+### Entities
+- Model
+  - Player --> id, playerName, color
+  - Cell --> row, col, Piece
+  - Piece --> piecetype, Color, validMove()
+  - Board --> Cell[8][8], 
+- Enum
+  - PieceType
+  - ColorType
+- Service
+  - ChessService --> Player[], board
---
+++ chess-validator/src/main/java/model/King.java
@@ -0,0 +1,32 @@
+package model;
+
+import enums.ColorType;
+import enums.PieceType;
+
+public class King extends Piece {
+    public King(ColorType colorType) {
+        super(PieceType.KING, colorType);
+    }
+
+    @Override
+    public boolean isValidMove(Board board, Cell start, Cell end) {
+        // move one step in any direction (horizontally, vertically, or diagonally).
+        int[] dx = new int[]{-1, -1, -1, 0, 1, 1, 1, 0};
+        int[] dy = new int[]{-1, 0, 1, 1, 1, 0, -1, -1};
+
+        int[] startCoordinates = board.getIndexByPosition(start);
+        int [] endCoordinates = board.getIndexByPosition(end);
+
+        if(startCoordinates[0] == -1 || startCoordinates[1] == -1 || endCoordinates[0] == -1 || endCoordinates[1] == -1)
+            return false;
+
+        boolean isValid = false;
+
+        for (int i = 0; i < dx.length; i++) {
+            if ((startCoordinates[0] + dx[i] == endCoordinates[0]) && (startCoordinates[1] + dy[i] == endCoordinates[1]))
+                isValid = true;
+        }
+
+        return isValid && isValidColorMove(start.getPiece(), end.getPiece());
+    }
+}
